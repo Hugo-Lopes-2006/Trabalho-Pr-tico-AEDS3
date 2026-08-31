1fe6232e033323f7270e984ff49f4a6923579e02
hugolopes436@gmail.com <hugolopes436@gmail.com>
2026-08-31 17:32:49 -0300

    subindo csv e passando o obj

---
+++ Objet.java
@@ -0,0 +1,14 @@
+public class Objet {
+    String id;
+    String artists;
+    String title;
+    int popularity;
+
+    public Objet(String id, String artists, String title, int popularity) {
+        this.id = id;
+        this.artists = artists;
+        this.title = title;
+        this.popularity = popularity;
+    }
+
+}
