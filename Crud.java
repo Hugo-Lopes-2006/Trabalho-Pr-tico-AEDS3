85305468a1dfcfab061da20a278ff48bca0864d4
Rafael Senna <rafaelsenna236@gmail.com>
2026-09-06 00:36:26 -0300

    versao final

--- Crud.java
+++ Crud.java
@@ -65,16 +65,15 @@ class Crud {
             if(lapide==0){//se nao estiver removido
                 Objeto objeto=new Objeto();
                 objeto.fromByteArray(ba);//cria objeto com array de bytes
-                System.out.println("ID lido do arquivo: " + objeto.getId() + " | ID procurado: " + id);
                 if(objeto.getId()==id){//se for o objeto
                     byte[] ba1=objetoAtualizado.toByteArray();//cria array de bytes do objeto novo
                     if(ba1.length<=ba.length){//se o novo couber no espaço do antigo
                         raf.seek(p1);
                         raf.write(ba1);
-                    }else{ // novo nao cabe
-                        delete(id); // deleta o antigo
-                        raf.seek(raf.length()); // vai pro final do arquivo
-                        LeitorCsv.escrever(raf, objetoAtualizado, id); // escreve no final o novo objetoAtualizado
+                    }else{// novo nao cabe
+                        delete(id);// deleta o antigo
+                        raf.seek(raf.length());// vai pro final do arquivo
+                        LeitorCsv.escrever(raf, objetoAtualizado, id);//escreve no final o novo objetoAtualizado
                     }
                     found=true;
                 }
